7905555796b3489c64e9e42abf025eba402462f8
sunny <sunny@jbcole.co.uk>
2025-06-06 15:55:10 +0100

    Updated server config

--- worker/index.js
+++ worker/index.js
@@ -3,7 +3,7 @@ const redis = require('redis');
 
 const redisClient = redis.createClient({
   host: keys.redisHost,
-  port: keys.redisPorts,
+  port: keys.redisPort,
   retry_strategy: () => 1000
 });
 
@@ -12,7 +12,7 @@ const sub = redisClient.duplicate();
 const fib = (index) => index < 2 ? 1 : fib(index - 1) + fib(index - 2);
 
 sub.on('message', (channel, message) => {
-  redisClient.hSet('values', message, fib(parseInt(message)))
+  redisClient.hset('values', message, fib(parseInt(message)))
 });
 
 sub.subscribe('insert');
--- worker/keys.js
+++ worker/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  redisHost: process.env.REDIS_HOST || '127.0.0.1',
-  redisPorts: process.env.REDIS_PORTS || '8000',
+  redisHost: process.env.REDIS_HOST,
+  redisPort: process.env.REDIS_PORT,
 }
